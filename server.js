c6c2f61cff449477d6bcc869ab9ec657af94ab6f
MeshachB <MeshachCBrown@gmail.com>
2025-07-18 13:21:58 -0400

    conneted  mongodb

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 // Here is where we import modules
 // We begin by loading Express
+const dotenv = require('dotenv');
+dotenv.config();
+
 const express = require('express');
+const mongoose = require('mongoose');
+const { connected } = require('process');
 
 const app = express();
 
-
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on('connected',() =>{
+ console.log('Connected to MongoDB')  
+})
 
 // server.js
 
